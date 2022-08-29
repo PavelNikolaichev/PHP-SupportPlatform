927df7a0c9b46b5a7c8ac0f6931d5717d44b14e9
PavelNikolaichev <pavelnikolaichev@gmail.com>
2022-08-29 09:33:04 +0300

    [WIP][NotTested] Made message model with controller and seeding.

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\TicketsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,13 +19,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// TODO: swap those plugs for a real controller methods.
 Route::get('tickets', [TicketsController::class, 'index']);
-
 Route::get('tickets/{ticket}', [TicketsController::class, 'show']);
-
 Route::post('tickets', [TicketsController::class, 'store']);
-
 Route::put('tickets/{ticket}', [TicketsController::class, 'update']);
-
 Route::delete('tickets/{ticket}', [TicketsController::class, 'delete']);
+
+Route::get('messages', [MessageController::class, 'index']);
+Route::get('messages/{message}', [MessageController::class, 'show']);
+Route::post('messages', [MessageController::class, 'store']);
+Route::put('messages/{message}', [MessageController::class, 'update']);
+Route::delete('messages/{message}', [MessageController::class, 'delete']);
