d8bcad008b130caa2e1a6b6c602721cac1e0a28d
PavelNikolaichev <pavelnikolaichev@gmail.com>
2022-09-26 16:42:09 +0300

    Cleared some crappy commented/unused code.

--- src/resources/js/components/Login.jsx
+++ src/resources/js/components/Login.jsx
@@ -29,11 +29,9 @@ export class Login extends Component {
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                         Password
                     </label>
-                    {/*border-red-500 to input class if error*/}
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                         id="password" type="password" placeholder="******************" onChange={(e) => this.handleInput('password', e)}/>
-                        {/*<p className="text-red-500 text-xs italic invisible">Please choose a password.</p>*/}
                 </div>
                 <div className="flex items-center justify-between">
                     <button
--- src/resources/js/components/Registration.jsx
+++ src/resources/js/components/Registration.jsx
@@ -30,21 +30,17 @@ export class Registration extends Component {
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
                         Name
                     </label>
-                    {/*border-red-500 to input class if error*/}
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                         id="name" type="text" placeholder="John" onChange={(e) => this.handleInput('name', e)}/>
-                    {/*<p className="text-red-500 text-xs italic invisible">Please choose a password.</p>*/}
                 </div>
                 <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                         Password
                     </label>
-                    {/*border-red-500 to input class if error*/}
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                         id="password" type="password" placeholder="******************" onChange={(e) => this.handleInput('password', e)}/>
-                    {/*<p className="text-red-500 text-xs italic invisible">Please choose a password.</p>*/}
                 </div>
                 <div className="flex items-center justify-between">
                     <button
@@ -54,28 +50,6 @@ export class Registration extends Component {
                     </button>
                 </div>
             </form>
-                // <form onSubmit={this.handleRegister}>
-                //     <div className="form-group">
-                //         <label htmlFor="email" className="col-form-label-lg">Email</label>
-                //         <input className="form-control" type="text" placeholder="example@gmail.com"
-                //                onChange={(e) => this.handleInput('email', e)}/>
-                //     </div>
-                //     <div className="form-group">
-                //         <label htmlFor="name" className="col-form-label-lg">Name</label>
-                //         <input className="form-control" type="text" placeholder="Username"
-                //                onChange={(e) => this.handleInput('name', e)}/>
-                //     </div>
-                //     <div className="form-group">
-                //         <label htmlFor="password" className="col-form-label-lg">Password</label>
-                //         <input className="form-control" type="password" placeholder="Password"
-                //                onChange={(e) => this.handleInput('password', e)}/>
-                //     </div>
-                //     <button
-                //         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                //         type="submit">
-                //         Register
-                //     </button>
-                // </form>
         )
     }
 
